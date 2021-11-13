09619aea108def28e20eb69f75d59ea2f8169250
lasallejs <janisa_lasalle@yahoo.com>
2021-11-13 10:45:19 -0500

    university of minn

---
+++ 19.2 activities/Example_code/index.js
@@ -0,0 +1,33 @@
+console.log("this is a test");
+
+
+
+// var myNumber=10;
+
+// if (myNumber > 10) { 
+
+//     console.log("Your number is greater than 10");
+// }
+
+// else if (myNumber == 10) {
+//     console.log("Your number equals 10");
+// }
+
+// else { 
+//     console.log("It is not greater than 10");
+
+// }
+
+var myName ="Janisa";
+
+var yourName="Svetlana";
+
+
+if (myName == yourName) {
+    console.log("We have the same name");
+
+}
+
+else {
+    console.log("We don't have the same name");
+}
